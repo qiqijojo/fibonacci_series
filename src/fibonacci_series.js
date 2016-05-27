1af4932074c03135ad5e8ad1aa7a85b4730094a4
wwwq77 <774948439@qq.com>
2016-05-27 23:26:58 +0800

    fibonacci提交

--- src/fibonacci_series.js
+++ src/fibonacci_series.js
@@ -1,7 +1,16 @@
 'use strict';
 
 function fibonacci_series(n) {
-
+	var a=0,b=1,res;
+	var arr = [];
+	arr.push(a);
+	arr.push(b);
+	for(var i=2;i<=n;i++){
+		res=a+b;
+		a=b;
+		b=res;
+		arr.push(res);
+		}
+		return arr;
 }
-
 module.exports = fibonacci_series;
